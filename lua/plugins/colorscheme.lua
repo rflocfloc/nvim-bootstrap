feee3e604002839da8d1ad71dab62ee752f64475
rflocfloc <riccardoscandino@gmail.com>
2025-08-06 15:56:41 +0200

    colorscheme update to vauge

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,8 +2,9 @@
 vim.pack.add({
   { src = "https://github.com/wnkz/monoglow.nvim.git", priority = 1000},
   { src = "https://github.com/rose-pine/neovim.git", priority = 1000, name="rose-pine"},
+  { src = "https://github.com/vague2k/vague.nvim.git", priority = 1000},
 })
 
 -- [[ Enable colorscheme ]]
 vim.o.background = 'dark'
-vim.cmd.colorscheme 'rose-pine'
+vim.cmd.colorscheme 'vague'
