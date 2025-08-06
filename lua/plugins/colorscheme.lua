cb293551befefb663089414fc0d0c912f0800493
rflocfloc <riccardoscandino@gmail.com>
2025-08-06 10:48:58 +0200

    rosepine colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,9 @@
 -- [[ Add Plugin ]]
 vim.pack.add({
   { src = "https://github.com/wnkz/monoglow.nvim.git", priority = 1000},
+  { src = "https://github.com/rose-pine/neovim.git", priority = 1000, name="rose-pine"},
 })
 
 -- [[ Enable colorscheme ]]
 vim.o.background = 'dark'
-vim.cmd.colorscheme 'monoglow'
+vim.cmd.colorscheme 'rose-pine'
