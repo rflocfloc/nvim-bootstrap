2d2e6989167050988ddeecd97f5255dff7381ebf
rflocfloc <riccardoscandino@gmail.com>
2026-04-21 14:13:39 +0200

    removed unnecessary keymaps, options with descriptions

--- init.lua
+++ init.lua
@@ -1,77 +1,81 @@
 -- ~/.config/nvim/init.lua
 
--- -------------
+-- -----------------------------------------
 -- [[ OPTIONS ]]
--- -------------
 
--- Set leader key
--- very useful for custom keymaps (eg. '<leader>X')
+-- Set leader key to <Space>
 vim.g.mapleader =  " "
 
--- Basic settings
+-- show line numbers and relative numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.wrap = false
-vim.opt.inccommand = 'split'
-vim.opt.confirm = true
 
--- Indentation
+-- set default indentation
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
+
+-- use spaces when inserting tabs
 vim.opt.expandtab = true
 
--- Search settings
-vim.opt.smartcase = true
+-- show whitespaces
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '·' }
 
--- Loclist default location
+-- incremental command preview
+vim.opt.inccommand = 'split'
+
+-- ask confirm when leaving unsaved files
+vim.opt.confirm = true
+
+-- open windows right/low
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
--- Visual settings
-vim.opt.termguicolors = true
-vim.opt.guicursor = ""
-vim.opt.signcolumn = "yes"
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+-- Case insensitive searching UNLESS /C or the search has capitals.
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
--- File handling/history
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+-- Save undo history
 vim.opt.undofile = true
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+
+-- visual settings
+vim.opt.termguicolors = true
+
+-- keep signcolumn on by default
+vim.opt.signcolumn = "yes"
+
+-- update times and timeouts (for performance)
 vim.opt.updatetime = 300
 vim.opt.timeoutlen = 500
+vim.opt.ttimeoutlen = 10
 
--- File Brower settings (Netrw)
--- vim.g.netrw_banner = 0	-- Uncomment to remove Netrw banner
--- vim.g.netrw_liststyle = 3  -- Uncomment to have tree style :Explore
-vim.g.netrw_showhidden = 1
-vim.g.netrw_winsize = 20
-
--- ------------------
+-- -----------------------------------------
 -- [[ AUTOCOMMANDS ]]
--- ------------------
+
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking text',
-  group = vim.api.nvim_create_augroup("User_HighlightText", { clear = true }),
+  group = vim.api.nvim_create_augroup("User/HighlightText", { clear = true }),
   callback = function()
     vim.hl.on_yank()
   end,
 })
 
--- -------------
+-- -----------------------------------------
 -- [[ KEYMAPS ]]
--- -------------
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', {desc = "<Esc> to clear search highlights"})
-vim.keymap.set('t', '<Esc>', '<C-\\><C-N>', {desc = "<Esc> to exit terminal mode"})
 
-vim.keymap.set({'v', 'x'}, '<S-h>', '<gv', { noremap = true, silent = true, desc = 'Move selection to the left' })
-vim.keymap.set({'v', 'x'}, '<S-l>', '>gv', { noremap = true, silent = true, desc = 'Move selection to the right' })
-vim.keymap.set({'v', 'x'}, '<S-j>', ':m \'>+1<CR>gv=gv', { noremap = true, silent = true, desc = 'Move block selection down' })
-vim.keymap.set({'v', 'x'}, '<S-k>', ':m \'<-2<CR>gv=gv', { noremap = true, silent = true, desc = 'Move block selection up' })
+-- powerful Esc
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = "<Esc> to clear search highlights" })
+vim.keymap.set('t', '<Esc>', '<C-\\><C-N>', { desc = "<Esc> to exit terminal mode" })
 
--- Very useful default keymaps:
+-- indent while remaining in visual mode
+vim.keymap.set('x', '<', '<gv', { noremap = true, silent = true, desc = 'Move block selection left' })
+vim.keymap.set('x', '>', '>gv', { noremap = true, silent = true, desc = 'Move block selection right' })
+
+-- copy/paste and delete system integration
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to system clipboard" })
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete to blackhole" })
 vim.keymap.set("v", "<leader>p", [["_dP]], { desc = "Delete to blackhole and Paste" })
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to system clipboard" })
+
+-- quick global file replace
 vim.keymap.set("v", "<leader>r", [["hy:%s/<C-r>h//g<left><left>]], { desc = "Replace selection globally" })
