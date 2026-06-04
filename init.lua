05fa672b22bf27c6d56a0620fe903c3060d500db
rflocfloc <riccardoscandino@gmail.com>
2026-06-04 14:34:59 +0200

    Added 1 plugin "mini.clue" for easier keymaps remembering

--- init.lua
+++ init.lua
@@ -1,18 +1,58 @@
--- ~/.config/nvim/init.lua
 
--- -----------------------------------------
--- [[ OPTIONS ]]
+-----------------------[ Globals ]-----------------------
 
--- Set leader key to <Space>
+-- set leader key to <Space>
+-- in some cases it is better to have it at the start of the config
 vim.g.mapleader =  " "
+
+-----------------------[ Plugins ]-----------------------
+
+-- download plugins with built-in vim.pack (nvim >= 0.12, remove lines if < 0.12)
+-- you don't really need plugins, mini.clue helps you remember keymaps
+-- comment/delete the entire section if you don't want it
+
+vim.pack.add({
+  { src = "https://github.com/nvim-mini/mini.clue.git" }
+})
+
+-- loading and configuring mini.clue
+local miniclue = require 'mini.clue'
+miniclue.setup {
+  triggers = {
+    { mode = { 'n', 'x' }, keys = 'g' },
+    { mode = { 'n', 'x' }, keys = '`' },
+    { mode = { 'n', 'x' }, keys = '"' },
+    { mode = { 'n', 'x' }, keys = 'z' },
+    { mode = { 'i', 'c' }, keys = '<C-r>' },
+    { mode = 'n', keys = '<C-w>' },
+    { mode = 'i', keys = '<C-x>' },
+    { mode = { 'n', 'x' }, keys = '<leader>' },
+    { mode = 'n', keys = '[' },
+    { mode = 'n', keys = ']' },
+  },
+  clues = {
+    miniclue.gen_clues.g(),
+    miniclue.gen_clues.z(),
+    miniclue.gen_clues.marks(),
+    miniclue.gen_clues.registers(),
+    miniclue.gen_clues.windows(),
+  },
+  window = {
+    delay = 500,
+    scroll_down = '<C-f>',
+    scroll_up = '<C-b>',
+  },
+}
+
+-----------------------[ Options ]-----------------------
 
 -- show line numbers and relative numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
 
 -- set default indentation
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 
 -- use spaces when inserting tabs
 vim.opt.expandtab = true
@@ -21,13 +61,13 @@ vim.opt.expandtab = true
 vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '·' }
 
--- incremental command preview
+-- incremental command preview when search/replace
 vim.opt.inccommand = 'split'
 
 -- ask confirm when leaving unsaved files
 vim.opt.confirm = true
 
--- open windows right/low
+-- open windows right/low as default
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
@@ -42,7 +82,7 @@ vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 -- visual settings
 vim.opt.termguicolors = true
 
--- keep signcolumn on by default
+-- keep signcolumn on by default (:h signcolumn)
 vim.opt.signcolumn = "yes"
 
 -- update times and timeouts (for performance)
@@ -50,8 +90,7 @@ vim.opt.updatetime = 300
 vim.opt.timeoutlen = 500
 vim.opt.ttimeoutlen = 10
 
--- -----------------------------------------
--- [[ AUTOCOMMANDS ]]
+-----------------------[ Autocommands ]-----------------------
 
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking text',
@@ -61,21 +100,20 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
--- -----------------------------------------
--- [[ KEYMAPS ]]
+-----------------------[ Keymaps ]-----------------------
 
 -- powerful Esc
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = "<Esc> to clear search highlights" })
 vim.keymap.set('t', '<Esc>', '<C-\\><C-N>', { desc = "<Esc> to exit terminal mode" })
 
--- indent while remaining in visual mode
+-- indent while remaining in visual mode, keeps original mapping < or >
 vim.keymap.set('x', '<', '<gv', { noremap = true, silent = true, desc = 'Move block selection left' })
 vim.keymap.set('x', '>', '>gv', { noremap = true, silent = true, desc = 'Move block selection right' })
 
--- copy/paste and delete system integration
+-- life-saving copy/paste to system clipboard (and delete)
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to system clipboard" })
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete to blackhole" })
 vim.keymap.set("v", "<leader>p", [["_dP]], { desc = "Delete to blackhole and Paste" })
 
--- quick global file replace
+-- quick global selected file replace (: %s/old/new command)
 vim.keymap.set("v", "<leader>r", [["hy:%s/<C-r>h//g<left><left>]], { desc = "Replace selection globally" })
